c6713e3ad799662e85787bad50baa206cc42a8aa
hyfaqy <hyfaqy@gmail.com>
2014-07-21 16:24:01 +0800

    可以显示人员的全部信息。

--- src/main/java/com/baldurtech/ContactServlet.java
+++ src/main/java/com/baldurtech/ContactServlet.java
@@ -28,9 +28,18 @@ public class ContactServlet extends HttpServlet{
 		try{
 			connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
 			statement = connection.createStatement();
-			resultset = statement.executeQuery("select * from contact where id =" + request.getParameter("contactName"));
+			resultset = statement.executeQuery("select * from contact where id =" + request.getParameter("contactId"));
 			resultset.next();
-			response.getWriter().println(resultset.getString("name"));
+			response.getWriter().println("name:" + resultset.getString("name"));
+			response.getWriter().println("mobile:" + resultset.getString("mobile"));
+			response.getWriter().println("vpmn:" + resultset.getString("vpmn"));
+			response.getWriter().println("email:" + resultset.getString("email"));
+			response.getWriter().println("homeaddress:" + resultset.getString("home_address"));
+			response.getWriter().println("officeaddress:" + resultset.getString("office_address"));
+			response.getWriter().println("memo:" + resultset.getString("memo"));
+			response.getWriter().println("groups:" + resultset.getString("groups"));
+			response.getWriter().println("job:" + resultset.getString("job"));
+			response.getWriter().println("joblevel:" + resultset.getInt("job_level"));
 		}catch(SQLException sqle){
 			response.getWriter().println("Cannot connect to DB.");
 			response.getWriter().println(sqle.getMessage());
