9db8b24d791d75dc39197f32757aa524b9ea134a
hyfaqy <hyfaqy@gmail.com>
2014-07-21 11:55:10 +0800

    新的

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ mkdir -p $CATALINA_HOME/webapps/fuzzy-tribble
 
 cp -r src/main/webapp/* $CATALINA_HOME/webapps/fuzzy-tribble
 cp -r target/classes/ $CATALINA_HOME/webapps/fuzzy-tribble/WEB-INF/
+cp -r lib $CATALINA_HOME/webapps/fuzzy-tribble/WEB-INF/
