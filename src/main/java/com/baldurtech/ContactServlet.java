d6b84b2ed5fc96f3c4627fcb1a1d1b5150e4a742
hyfaqy <hyfaqy@gmail.com>
2014-07-21 11:22:24 +0800

    添加了数据库代码

--- src/main/java/com/baldurtech/ContactServlet.java
+++ src/main/java/com/baldurtech/ContactServlet.java
@@ -3,23 +3,30 @@ package com.baldurtech;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 import java.io.IOException;
+import javax.servlet.ServletException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ContactServlet extends HttpServlet{
-	public void doGet(HttpServletRequest req,
-					  HttpServletResponse resp)
-					  throws IOException,ServletException{
-		resp.getWriter().println("OK");			  
-	
-	}
-	public void doPost(HttpServletRequest req,
-					   HttpServletResponse resp)
-					   throws IOException,ServletException{
-		String username = req.getParameter("username");	
-		String password = req.getParameter("password");
+	public	void doGet(HttpServletRequest request, HttpServletResponse response)
+						throws IOException,ServletException {
+		response.getWriter().println("Get contact by id: " + request.getParameter("contactId"));
 		
-		resp.getWriter().println("username:" + username);
-		resp.getWriter().println("password:" + password);
-	}
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		}catch(Exception ex){
+		
+		}
+		try{
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test?user=root&password=");
+			response.getWriter().println(connection);
+			connection.close();
+		}catch(SQLException sqle){
+			response.getWriter().println("Cannot connect to DB.");
+			response.getWriter().println(sqle.getMessage());
+			sqle.printStackTrace();
+		}
+	}											
 }
